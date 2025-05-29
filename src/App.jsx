f7403b6d8ac98dc19d8ff2cd6cf509aea1f8ea3d
jayme <jayme35571@gmail.com>
2025-05-29 15:42:39 -0500

    chore - refactor

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import {
   Link,
   Route,
   Routes,
-  useLocation,
-  useNavigate,
   useParams,
 } from "react-router-dom";
+import Header from "./components/header.jsx"
+
 /*para este proyecto tambien se puede
 usar props con useLocation() react router dom
 const propsParaPasar = {
@@ -54,73 +54,20 @@ const options = {
 //cuenta 2
 //jayme35371@gmail.com
 
+/*--pueden usar mi api key es totalmente gratis he aqui la razon por la cual 
+no veo la necesidad de usar una variable de entorno convencional
+--*/
+
+const __apiKey = "7e77b90097msh8b79d681a94027cp13c706jsna21961598a5e"
+const __host = "youtube-search-and-download.p.rapidapi.com"
+
 const options = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "7e77b90097msh8b79d681a94027cp13c706jsna21961598a5e",
-    "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com",
+    "X-RapidAPI-Key": __apiKey,
+    "X-RapidAPI-Host":__host,
   },
 };
-
-function Header() {
-  const navigate = useNavigate();
-
-  /*aqui "search" se ejecuta
-  cada vez que se da click en la lupa(buscar)*/
-  const search = (value) => {
-    const recorte = value.replace(/\s+/g, " "); //aqui usamos regex remplazamos si es que tiene mas de un espacio a solo un espacio
-    navigate(`/search/${recorte}`);
-  };
-  const inputHeader = useRef();
-
-  //aqui si presiona tecla enter va a  redirigir a la busqueda
-  const boton = (e) => {
-    /*e.keyCode es por si el usuario presiono la tecla "enter"
-    que tiene como numero de ascci 13
-    */
-    if (e.keyCode == 13) {
-      const lupa = document.querySelector(".lupa");
-      lupa.click();
-    }
-  };
-  return (
-    <div className="header">
-      <Link to="/" style={{ textDecoration: "none" }}>
-        <div className="logo">
-          <div className="header-image">
-            <img src="/youtube-icono.png"></img>
-          </div>
-          <span className="name">videos</span>
-        </div>
-      </Link>
-      <div className="search">
-        <input
-          type="text"
-          className="input"
-          ref={inputHeader}
-          placeholder="Buscar"
-          onKeyDown={boton}
-        />
-        <div
-          className="lupa"
-          onClick={() => search(inputHeader.current.value)}
-        >
-          <svg
-            focusable="false"
-            aria-hidden="true"
-            viewBox="0 0 24 24"
-            data-testid="SearchIcon"
-          >
-            <path
-              d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"
-            >
-            </path>
-          </svg>
-        </div>
-      </div>
-    </div>
-  );
-}
 
 //estos son los iconos mas su nombre, es el NAV
 const objeto = [
@@ -794,10 +741,8 @@ function Search() {
   );
 }
 
-/*  <div className="titulo">
-          <span className="tituloName">videos de</span>
-          <span className="tituloBusqueda">{parametro}</span>
-        </div>*/
+
+
 
 function Channel() {
   const { idChannel } = useParams();
---
+++ src/components/header.jsx
@@ -0,0 +1,64 @@
+import { useNavigate,Link } from "react-router-dom"
+import {useRef} from "react"
+
+function Header() {
+    const navigate = useNavigate();
+
+    /*aqui "search" se ejecuta
+    cada vez que se da click en la lupa(buscar)*/
+    const search = (value) => {
+        const recorte = value.replace(/\s+/g, " "); //aqui usamos regex remplazamos si es que tiene mas de un espacio a solo un espacio
+        navigate(`/search/${recorte}`);
+    };
+    const inputHeader = useRef();
+
+    //aqui si presiona tecla enter va a  redirigir a la busqueda
+    const boton = (e) => {
+        /*e.keyCode es por si el usuario presiono la tecla "enter"
+        que tiene como numero de ascci 13
+        */
+        if (e.keyCode == 13) {
+            const lupa = document.querySelector(".lupa");
+            lupa.click();
+        }
+    };
+    return (
+        <div className="header">
+            <Link to="/" style={{ textDecoration: "none" }}>
+                <div className="logo">
+                    <div className="header-image">
+                        <img src="/youtube-icono.png"></img>
+                    </div>
+                    <span className="name">videos</span>
+                </div>
+            </Link>
+            <div className="search">
+                <input
+                    type="text"
+                    className="input"
+                    ref={inputHeader}
+                    placeholder="Buscar"
+                    onKeyDown={boton}
+                />
+                <div
+                    className="lupa"
+                    onClick={() => search(inputHeader.current.value)}
+                >
+                    <svg
+                        focusable="false"
+                        aria-hidden="true"
+                        viewBox="0 0 24 24"
+                        data-testid="SearchIcon"
+                    >
+                        <path
+                            d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"
+                        >
+                        </path>
+                    </svg>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Header
